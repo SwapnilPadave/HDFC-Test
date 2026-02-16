3f9299bdfd46e516a2ee84e6dd6c0e92238141d3
spadave7 <spadave7@gmail.com>
2026-02-16 16:06:26 +0530

    practice on jquery and ajax in the project.

---
+++ MachinTestForHDFC/Dtos/GetDepartmentsDto.cs
@@ -0,0 +1,8 @@
+﻿namespace MachinTestForHDFC.Dtos
+{
+    public class GetDepartmentsDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+    }
+}
